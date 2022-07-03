b1e3106bf0d797779feffa87b8cba5281f886fb6
Scott <scottjiang19@gmail.com>
2022-07-03 00:36:32 -0600

    work on member list page

--- member_management_frontend/src/App.js
+++ member_management_frontend/src/App.js
@@ -1,46 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useEffect, useState} from 'react'
+import logo from "./logo.svg";
+import "./App.scss";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link, useNavigate, Route } from "react-router-dom";
+import userAvatar from "./user-avatar.svg";
+import IconButton from "@mui/material/IconButton";
+import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 
 
 function App() {
-  const [members, setMembers] = useState('')
+	const [members, setMembers] = useState([]);
+	let navigate = useNavigate();
 
-  const newMember = {
-    id: 10,
-    first_name:"bob",
-    last_name: "joe",
-    user_email: "joegolghsdfsdfjghjghd@gmail.com",
-    user_phone: "3242345",
-    user_role: "REG"
-  }
+	const newMember = {
+		id: 15,
+		first_name: "Bob",
+		last_name: "Jones",
+		user_email: "sdfsfsdfdddffdf@gmail.com",
+		user_phone: "3242345",
+		user_role: "ADM",
+	};
 
-  const handleSubmit = () => {
-    axios
-      .get("http://localhost:8000/api/team_management/")
-      .then((res) => console.log(res.data))
-      .catch((err) => console.log(err))
-  }
-  const handleCreate = () => {
-    axios
-      .post("http://localhost:8000/api/team_management/", newMember)
-      .then((res) => console.log(res.data))
-      .catch((err) => console.log(err))
-  }
+	useEffect(() => {
+		axios
+			.get("http://localhost:8000/api/team_management/")
+			.then((res) => {
+				console.log(res.data);
+				setMembers(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
+	const handleCreate = () => {
+		axios
+			.post("http://localhost:8000/api/team_management/", newMember)
+			.then((res) => console.log(res.data))
+			.catch((err) => console.log(err));
+	};
 
-  return (
-    <div className="App">
-      <header className="App-header">
-          <button onClick={handleSubmit}> click here! </button>
-          <button onClick={handleCreate}> CREATE! </button>
-          <div>
-            {members}
-          </div>
-      </header>
-    </div>
-  );
+	const handleNavigate = (userId) => {
+		navigate("/edituser", { state: { id: userId } });
+	};
+
+	return (
+		<div className="App">
+			{/* <button onClick={handleCreate}> CREATE! </button> */}
+			<div className="header">
+				<div className="header-description">
+					<h1> Team Members </h1>
+					<span>
+						{" "}
+						You have {members.length}{" "}
+						{members.length === 1 ? "member" : "members"}
+					</span>
+				</div>
+				<div className="header-add">
+					<Button variant="contained" startIcon={<AddIcon />} onClick={handleCreate}>
+						Add
+					</Button>
+				</div>
+			</div>
+
+			{members.map((member) => (
+				<div className="user-card" onClick={() => handleNavigate(member.id)}>
+					<div className="user-avatar" />
+					<div className="user-info">
+						<span>
+							{" "}
+							<b>
+								{member.first_name} {member.last_name}{" "}
+								{member.user_role === "ADM" && "(Admin)"}
+							</b>
+						</span>
+						<span> {member.user_phone} </span>
+						<span> {member.user_email} </span>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
--- member_management_frontend/src/index.js
+++ member_management_frontend/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import AddUser from './Components/AddUser';
+import EditUser from './Components/EditUser';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+     <Route path="/" element={<App />} />
+     <Route path="/adduser" element={<AddUser />} />
+     <Route path="/edituser" element={<EditUser />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
